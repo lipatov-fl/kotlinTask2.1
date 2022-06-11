315d578a03207dbb56d55bfcc4eda6b7684dcd41
lipatov-fl <lipatov-fl@yandex.ru>
2022-06-11 20:19:07 +0400

    Добавлен файл .gitignore и решение задачи

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val amount: Int = 55000
+    val minTranslation: Int = 35
+    val minCommission: Double = 0.75
+
+    val amountCop: Int = amount * 100
+    val minTranslationCop: Int = minTranslation * 100
+
+    val sumCommission : Double = amountCop / 100 * 0.75
+
+
+    if(sumCommission < minTranslationCop) {
+        println("Сумма коммисии равна $minTranslation руб.")
+    } else {
+        val sumCommissionRub: Double = sumCommission / 100
+        println("Сумма коммисии равна $sumCommissionRub руб.")
+    }
+}
